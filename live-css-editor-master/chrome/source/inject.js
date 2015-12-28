cedca6df8dec5d9c942d8b67a6102b93f1755ad6
Noah Lipsyc <n.lipsyc@gmail.com>
2015-12-28 12:31:01 -0500

    Split off source material.  Added in context menu for getting parent nodes of element.  Still debugging.

--- live-css-editor-master/chrome/source/inject.js
+++
@@ -1,9 +0,0 @@
-/* Tell the extension to load the existing CSS if there is any */
-(function () {
-  var url = document.location,
-    css = window.localStorage.getItem('livecsseditor-cache-' + url);
-
-  if (css && css !== '') {
-    chrome.extension.sendMessage({ modify : 'true' }, function (response) {});
-  }
-}());
